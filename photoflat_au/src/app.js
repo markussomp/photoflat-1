8ede0a2850993f52f6fd7d1d334f266d4b5ae9c9
rosulg <robinsulg@live.com>
2017-03-02 17:41:33 +0200

    Add files via upload

--- photoflat_au/src/app.js
+++ photoflat_au/src/app.js
@@ -7,7 +7,8 @@ export class App {
       { route: ['', 'home'],       name: 'home',       moduleId: 'home/index' },
       { route: 'people', name: 'people',  moduleId: 'people/people',   nav: true },
       { route: 'profile', name: 'profile',  moduleId: 'profile/profile',   nav: true },
-      { route: 'event', name: 'event',  moduleId: 'event/event',   nav: true }
+      { route: 'event', name: 'event',  moduleId: 'event/event',   nav: true },
+      { route: 'settings', name: 'settings',  moduleId: 'settings/settings',   nav: true }
 
     ]);
     config.mapUnknownRoutes('not-found');
